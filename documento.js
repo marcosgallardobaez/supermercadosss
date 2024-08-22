2380c66c570af59ed8b85aad31fbce080c4c6dd0
Marcos Luciano Gallardo Baez <marcos.gallardo.baez@gmail.com>
2024-08-21 20:34:36 -0400

    primer commit

---
+++ documento.js
@@ -0,0 +1,11 @@
+$('.formulario').mouseover(function() {
+    $('.formulario').css('background-color','orange')
+})
+    
+$('.formulario').mouseout(function() {
+    $('.formulario').css('background-color','greenyellow')
+})
+
+$('img').mouseover(function() {
+    $('img').css('transform', 'scale(1.2)', )
+})
